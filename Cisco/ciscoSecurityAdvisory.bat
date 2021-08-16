eb59ab1f9ab66e4bbccabfa22f1ab74d5f6faa99
mkchan671 <39592908+mkchan671@users.noreply.github.com>
2021-08-16 16:16:34 +0800

    Update ciscoSecurityAdvisory.bat

--- Cisco/ciscoSecurityAdvisory.bat
+++ Cisco/ciscoSecurityAdvisory.bat
@@ -1,2 +1,8 @@
-openVulnQuery --all --conf .\credentials.json --first_published 2021-05-01:2021-07-23 --csv .\cisco.csv
+Format - yyyy-mm-dd
+echo Date format = %date%
+echo dd = %date:~0,2%
+echo mm = %date:~3,2%
+echo yyyy = %date:~6,4%
+
+Python -m openVulnQuery --all --conf .\credentials.json --first_published %date:~6,4%-%date:~3,2%-01:%date:~6,4%-%date:~3,2%-%date:~0,2% --csv .\cisco.csv
 pause
